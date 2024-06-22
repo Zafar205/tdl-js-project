d9481f0de29c9ee09fbe30396e551b1cb4fdf7c6
Zafar205 <mz09206@st.habib.edu.pk>
2024-06-23 02:29:21 +0500

    add all tdl files

---
+++ scripts/todolist.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let tasks = JSON.parse(localStorage.getItem('permanenttask')) || [];
+
+    updateTasksDisplay();
+
+    function addTask() {
+        const textToAdd = document.querySelector(".js-input").value;
+        const dateToAdd = document.querySelector(".js-date").value;
+
+        tasks.push({ text: textToAdd, date: dateToAdd });
+
+        updateTasksDisplay();
+    }
+
+    function updateTasksDisplay() {
+        let tasksToDisplay = '';
+        for (let i = 0; i < tasks.length; i++) {
+            tasksToDisplay += `
+                <div class="task">
+                    <div>${tasks[i].text}</div>
+                    <div>${tasks[i].date}</div>
+                    <div><button onclick="deleteTask(${i})">Delete</button></div>
+                </div><br>`;
+        }
+        document.querySelector('.js-tasks').innerHTML = tasksToDisplay;
+
+        localStorage.setItem('permanenttask', JSON.stringify(tasks));
+    }
+
+    window.deleteTask = function(index) {
+        tasks.splice(index, 1);
+        updateTasksDisplay();
+    }
+
+    document.querySelector('.js-add').addEventListener('click', addTask);
+});
